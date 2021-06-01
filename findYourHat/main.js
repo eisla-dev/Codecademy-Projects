5d0a4ad9d3f17a2c849827f8cc4d2a327c900851
ajile <22140810+eisla-dev@users.noreply.github.com>
2021-05-31 22:11:57 -0600

    refactored to get rid of useless for loops

--- findYourHat/main.js
+++ findYourHat/main.js
@@ -6,14 +6,22 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-    constructor(fieldArrary) {
-        this._fieldArray = fieldArrary;   
+    constructor(fieldArray) {
+        this._fieldArray = fieldArray;
+        this._playerVerticalPosition = 0;
+        this._playerHorizontalPosition = 0;
     }
-    hatPosition(){
-        for (let i = 0; i<this._fieldArray.length; i++){
-            if (this._fieldArray[i].indexOf(hat) != -1){
-                console.log(`${i} ${this._fieldArray[i].indexOf(hat)}`)
-            }
+    winLoseCheck() {
+        let hPos;
+        let vPos;
+        if (this._fieldArray[this._playerVerticalPosition][this._playerHorizontalPosition] == hat) {
+            console.log('winner')
+            return true
+        } else if (this._fieldArray[this._playerVerticalPosition][this._playerHorizontalPosition] == hole) {
+            console.log('lose by hole')
+            return true
+        } else {
+            return false
         }
     }
     print() {
@@ -21,25 +29,55 @@ class Field {
             console.log(lines.join(''))
         }
     }
+    promptFunction() {
+        const input = prompt('Which way? ');
+        return input
+    }
+    drawPath(vPos, hPos){
+        if (this._fieldArray[vPos][hPos] != "^" && this._fieldArray[vPos][hPos] != hole)
+            this._fieldArray[vPos][hPos] = "*"
+    }
+    movePlayer(userInput) {
+        let direction=userInput.toUpperCase();
+        if (direction != 'U' && direction != 'D' && direction != 'L' && direction != 'R'){
+            console.log('Not a proper input')
+        } 
+        if (direction == 'U'){
+            this._playerVerticalPosition -= 1;
+            this.drawPath(this._playerVerticalPosition, this._playerHorizontalPosition);
+        }
+        if (direction == 'D'){
+            this._playerVerticalPosition += 1;
+            this.drawPath(this._playerVerticalPosition, this._playerHorizontalPosition);
+        }
+        if (direction == 'L'){
+            this._playerHorizontalPosition -= 1;
+            this.drawPath(this._playerVerticalPosition, this._playerHorizontalPosition);
+        }
+        if (direction == 'R'){
+            this._playerHorizontalPosition += 1;
+            this.drawPath(this._playerVerticalPosition, this._playerHorizontalPosition);
+        }
+    }
 }
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
 const myField = new Field([
   ['*', '░', 'O'],
   ['░', 'O', '░'],
   ['░', '^', '░'],
 ]);
-
-myField.print()
-myField.hatPosition()
-
-readline.question('Which way?', input => {
-    console.log(`inputted ${input}`);
-    readline.close();
-});
-
-
+do {
+    myField.print()
+    let userInput = myField.promptFunction();
+    myField.movePlayer(userInput);
+    //console.log(`Vertical Position: ${myField._playerVerticalPosition} \nHorizontal Position: ${myField._playerHorizontalPosition}`)
+} while (myField.winLoseCheck() == false)
+//this is a test for the functions based on other fields
+//const myField2 = new Field([
+//  ['*', '░', 'O', '░', '░'],
+//  ['░', 'O', '░', '░', '░'],
+//  ['░', '░', '░', '^', '░'],
+//]);
+//
+//myField2.print()
+//myField2.hatPosition()
